618121be503f116a8a53464b62cf8dc5fd9f2e89
10Artur <artyr11012002@gmail.com>
2023-09-26 19:31:11 +0300

    Update ContactsForm.jsx

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -1,79 +1,3 @@
-// import { Formik } from 'formik';
-// import * as Yup from 'yup';
-// import { nanoid } from 'nanoid';
-// import {
-//   ContactsError,
-//   ContactsField,
-//   ContactsForm,
-//   ContactsLabel,
-//   ContactsBtn,
-// } from './ContactsForm.styled';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { addContact } from 'redux/contactsSlice';
-
-// export const ContactForm = () => {
-//   const allContacts = useSelector(state => state.contactData.contacts);
-//   const dispatch = useDispatch();
-
-//   const ContactsFormSchema = Yup.object().shape({
-//     name: Yup.string()
-//       .test('is-unique', 'Name already exists', name => {
-//         const existingName = allContacts.find(
-//           contact => contact.name.toLowerCase() === name.toLowerCase()
-//         );
-//         return !existingName;
-//       })
-//       .required('Name is required!'),
-//     number: Yup.string().min(10).max(12).required('Number is required!'),
-//   });
-
-//   const handleSubmit = (values, action) => {
-//     const contact = {
-//       id: nanoid(),
-//       name: values.name,
-//       number: values.number,
-//     };
-
-//     dispatch(addContact(contact));
-//     action.resetForm();
-//   };
-
-//   return (
-//     <Formik
-//       initialValues={{
-//         name: '',
-//         number: '',
-//       }}
-//       validationSchema={ContactsFormSchema}
-//       onSubmit={handleSubmit}
-//     >
-//       <ContactsForm>
-//         <ContactsLabel>
-//           Name
-//           <ContactsField
-//             type="text"
-//             name="name"
-//             placeholder="Enter your name..."
-//           />
-//           <ContactsError component="div" name="name" />
-//         </ContactsLabel>
-
-//         <ContactsLabel>
-//           Number
-//           <ContactsField
-//             type="tel"
-//             name="number"
-//             placeholder="Enter your number..."
-//           />
-//           <ContactsError component="div" name="number" />
-//         </ContactsLabel>
-
-//         <ContactsBtn type="submit">Add contact</ContactsBtn>
-//       </ContactsForm>
-//     </Formik>
-//   );
-// };
-
 import { Formik, Field } from 'formik';
 import * as Yup from 'yup';
 import {
@@ -88,12 +12,12 @@ import { addContact } from 'redux/operations';
 import { PatternFormat } from 'react-number-format';
 
 export const ContactForm = () => {
-  const dispatch = useDispatch();
-
   const ContactsFormSchema = Yup.object().shape({
     name: Yup.string().required('Name is required!'),
-    number: Yup.string().min(10).max(12).required('Number is required!'),
+    number: Yup.string().min(10).max(20).required('Number is required!'),
   });
+
+  const dispatch = useDispatch();
 
   const handleSubmit = (values, action) => {
     const newContact = {
@@ -136,7 +60,7 @@ export const ContactForm = () => {
           <Field name="number">
             {({ field, form }) => (
               <PatternFormat
-                format="+1 (###) #### ###"
+                format="+380 (##) ## ## ###"
                 allowEmptyFormatting
                 mask="_"
                 value={form.values.number}
